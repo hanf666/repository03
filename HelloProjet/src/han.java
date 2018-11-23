a9e1102d5b22fbb0b02ffcc886a39747d1933453
hanf666 <896400596@qq.com>
2018-11-23 16:04:57 +0800

    afdasf

---
+++ HelloProjet/src/han.java
@@ -0,0 +1,11 @@
+public class han
+{
+
+    public static void main(String[] args) {
+        System.out.println("afdsfsfddafadf");
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
